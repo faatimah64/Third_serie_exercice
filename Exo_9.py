e30cf39fbf80ba0e5569642d9692bbe4f9ed5ba5
Fatoumata Binta Camara <fatoumacra0@gmail.com>
2022-05-23 16:30:28 +0000

    2ème Methode (utilisation des foctions)

--- Exo_9.py
+++ Exo_9.py
@@ -11,6 +11,39 @@ for i in Liste_mot:
          dict_mots[i] +=1
 print(dict_mots)
 
+#fonction permettant de Remplacer
+def A(ch):
+    modi=ch.replace("je veux", "Assim veut")
+    return modi
+print(A("je veux manger"))
+
+#fonction permettant de decouper en list de mots
+def B(mots):
+    res=mots.split()
+    return res
+print(B("je veux manger"))
+
+#fonction permettant de decouper en dictionaire de mots en mettant les mots comme clés et les occurences comme valeurs
+
+def C(lst):
+    dict_mots={}
+    for i in lst:
+        if i not in dict_mots.keys():
+            dict_mots[i]=1
+        else:
+            dict_mots[i] +=1
+    return dict_mots
+
+#affichage 1
+l1 = B("bonjour bonjour aurevoir")
+d1 = C(l1)
+print(d1)
+
+#affichage 2     
+print(C(B("bjr bjr bjr aur")))
+
+#(1=2)
+
 
 
 
